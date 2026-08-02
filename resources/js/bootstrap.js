e7e9470abca5dbd6df7ff60fcacd55757e6133c9
HumaCode <humaidizakaria1998@gmail.com>
2026-08-02 09:39:51 +0700

    feat: configure axios credentials and base URL for secure cross-origin requests

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+if (window.location.protocol === 'https:') {
+    window.axios.defaults.baseURL = window.location.origin;
+}
